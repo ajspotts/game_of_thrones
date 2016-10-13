495cfda8869d7b3c4b4e4e82c6679079c82ceb03
Alexander <ajspottswood@gmail.com>
2016-10-13 15:06:21 -0400

    New_functionality

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -8,8 +8,21 @@ class CharactersController < ApplicationController
   end
 
   def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.create!(character_params)
+
+    redirect_to character_path(@character)
   end
 
   def edit
+    @character = Character.find(params[:id])
+  end
+
+  private
+  def character_params
+    params.require(:character)
   end
 end
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -8,11 +8,21 @@ class HousesController < ApplicationController
   end
 
   def new
+    @house = House.new
+  end
+
+  def create
     @house = House.create!(house_params)
 
     redirect_to house_path(@house)
   end
 
   def edit
+    @house = House.find(params[:id])
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name, :img_url)
   end
 end
